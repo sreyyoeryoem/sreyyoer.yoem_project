e53ed03fdb1be616186628fb85f892a0dab59a12
YOEM.SREYYOER <YOEM.SREYYOER@PNC.LAN>
2022-11-29 15:12:51 +0700

    create a page choose seller or customer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
-let chooseUser = document.querySelector(".icon");
-console.log(chooseUser)
+let chooseUser = document.querySelector(".card_container");
+let homepage = document.querySelector(".manu_bar")
+console.log(homepage)
 
 // ------------------function----------------------------
-function show()
+function show(element){
+    element.style.display = "block";
+}
+function hide (element){
+    element.style.display = "none"
+}
+
+
+
+
+
+
+
+
+// ---------------------------------//-----------------------
+
+
+
+
+
+
+
+
+
+
+let iconChooseUser = document.querySelector(".icon");
+console.log(iconChooseUser)
+iconChooseUser.addEventListener("click", function(){
+    show(chooseUser);
+    hide(homepage)
+})
+
+// -----------------------------call function------------------------
+// show()
+// hide(homepage)
